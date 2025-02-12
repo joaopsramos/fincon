e54a893b244f1029085db8f3aa53b8630dde6d0d
João Ramos <joaopsramos231@gmail.com>
2025-02-12 07:15:25 -0300

    feat: define user salary on creation; salary can be updated

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -8,6 +8,8 @@ import Expense from "./expense";
 import { useNow } from "next-intl";
 import { useMemo } from "react";
 import { sortGoals } from "@/lib/utils";
+import Header from "./header";
+import Menu from "@/components/menu";
 
 export default function Index() {
   const date = useNow()
@@ -25,21 +27,29 @@ export default function Index() {
   }, [goals]);
 
   return (
-    <div className="m-4">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-        <div className="lg:col-span-2">
-          <Summary date={date} />
+    <>
+      <div className="m-4">
+        <div className="mb-2">
+          <Header date={date} />
         </div>
-        <div className="">
-          <Goals goals={sortedGoals || []} />
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          <div className="lg:col-span-2">
+            <Summary date={date} />
+          </div>
+          <div className="">
+            <Goals goals={sortedGoals || []} />
+          </div>
+        </div>
+
+        <div className="mt-2 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
+          {sortedGoals?.map(goal => (
+            <Expense key={goal.id} goal={goal} date={date} />
+          ))}
         </div>
       </div>
 
-      <div className="mt-2 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
-        {sortedGoals?.map(goal => (
-          <Expense key={goal.id} goal={goal} date={date} />
-        ))}
-      </div>
-    </div>
+      <Menu />
+    </>
   );
 }
