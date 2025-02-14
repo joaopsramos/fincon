37876328b59634aae74fe9d450115e6d9bec1631
João Ramos <joaopsramos231@gmail.com>
2025-02-14 16:36:21 -0300

    hotfix(frontend): add suspense boundary because of 'useSearchParams'

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -6,13 +6,27 @@ import Summary from "./summary"
 import { getGoals } from "@/api/goals"
 import Expense from "./expense"
 import { useNow } from "next-intl"
-import { useMemo } from "react"
+import { Suspense, useMemo } from "react"
 import { sortGoals } from "@/lib/utils"
 import Header from "./header"
 import Menu from "@/components/menu"
 import { useSearchParams } from "next/navigation"
 
-export default function Index() {
+export default function Dashboard() {
+  return (
+    <>
+      <div className="m-4">
+        <Suspense>
+          <Content />
+        </Suspense>
+      </div>
+
+      <Menu />
+    </>
+  )
+}
+
+function Content() {
   const searchParams = useSearchParams()
 
   const queryYear = searchParams.get("year") || new Date().getFullYear()
@@ -34,30 +48,27 @@ export default function Index() {
     return sortGoals(goals)
   }, [goals])
 
+
   return (
     <>
-      <div className="m-4">
-        <div className="mb-2">
-          <Header date={date} />
-        </div>
+      <div className="mb-2">
+        <Header date={date} />
+      </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-          <div className="lg:col-span-2">
-            <Summary date={date} />
-          </div>
-          <div className="">
-            <Goals goals={sortedGoals || []} />
-          </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+        <div className="lg:col-span-2">
+          <Summary date={date} />
         </div>
-
-        <div className="mt-2 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
-          {sortedGoals?.map((goal) => (
-            <Expense key={goal.id} goal={goal} date={date} />
-          ))}
+        <div className="">
+          <Goals goals={sortedGoals || []} />
         </div>
       </div>
 
-      <Menu />
+      <div className="mt-2 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
+        {sortedGoals?.map((goal) => (
+          <Expense key={goal.id} goal={goal} date={date} />
+        ))}
+      </div>
     </>
   )
 }
